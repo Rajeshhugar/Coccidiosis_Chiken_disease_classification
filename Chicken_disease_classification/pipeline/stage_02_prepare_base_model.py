7385cd7089c76dce2905f06f6a0d4ca1bd733ea2
Rajeshhugar <hugarrajesh@gmail.com>
2023-08-30 17:04:42 +0530

    added prepare base model

---
+++ Chicken_disease_classification/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,7 @@
+from Chicken_disease_classification.config.configuration import ConfigurationManager
+from Chicken_disease_classification.components.prepare_base_model import PrepareBaseModel
+from Chicken_disease_classification import logger
+
+STAGE_NAME = 'Prepare base Model'
+
+
